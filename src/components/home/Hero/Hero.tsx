f8b3fa887aeeacad71d03dc154127c0046b06863
quiroz557 <pablopersonal557@gmail.com>
2025-01-24 13:31:09 -0500

    Estilos para components

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,7 +1,10 @@
+import styles from './Hero.module.css';
+
 export const Hero = () => { 
     return (
-        <section>
+        <section className={styles.Hero}>
             <h1>Hero</h1>
+            <h2>Empowering your Tomorrow, Today!</h2>
         </section>
     )
 }
