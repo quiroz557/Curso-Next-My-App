e4874a099440070b9855b0e8bdbe027b947a258d
quiroz557 <pablopersonal557@gmail.com>
2025-01-25 13:10:37 -0500

    Creando nuevos estilos para description  y analizando a fondo la librería classnames para el control de las clases dinámicas

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,13 +1,44 @@
+"use client"
+
+import { useState } from 'react';
+import { PLACEHOLDER_IMAGE } from './constants';
 import styles from './Description..module.sass';
+import Image from 'next/image';
+import classNames from 'classnames/bind';
 
 export const Description = () => { 
+
+    const [hasBorder, setHasBorder] = useState(false);
+
+    const handleClick = () => setHasBorder(!hasBorder);
+
+    const context = classNames.bind(styles);
+
+    const buttonStyles = context('Description__button', {
+        'Description__button--border': hasBorder
+    })
+
     return (
         <section className={styles.Description}>
-            <img src="/images/description.jpeg" alt="products-marketplace" />
+
+            <button onClick={handleClick} className={buttonStyles}>
+                <div className={styles.Description__imageContainer}>   
+                    <Image 
+                        src="/images/description.jpeg" 
+                        alt="products-marketplace"  
+                        fill
+                        placeholder='blur'
+                        blurDataURL={PLACEHOLDER_IMAGE} 
+                    />
+                </div>
+            </button>
+
+
             <div className={styles.Description__text}>
                 <h2>Bring the future today</h2>
                 <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
             </div>
+
         </section>
     )
 }
